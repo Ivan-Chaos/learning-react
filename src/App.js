00f7cba49c5d8b338ac8ea54bfd51bc6d9948552
Ivan Chaus <ivan13oct@gmail.com>
2021-06-22 21:04:29 +0300

    #5 Adding components and whatnot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="contet">
-        <h1>I feel tre bon</h1>
+        <Home />
       </div>
     </div>
   );
