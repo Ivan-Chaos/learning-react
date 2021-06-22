d23836d4c3cd76bbb8adcc74aa2e929cef39eb3f
Ivan Chaus <ivan13oct@gmail.com>
2021-06-22 22:03:38 +0300

    #8 Hooks and stuff

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+    
+    const handleClick=()=>{
+        setName("Jebediah");
+        setAge(age+1);
+    }
+   
+    const [name, setName]=useState("Juan");
+    const [age, setAge]=useState(14);
+
     return ( 
         <div className="home">
             <h1>Home page</h1>
+            <p>{name} is {age} years yold</p>
+            <button onClick={ () => { handleClick(); }}>Backstab me</button>
         </div>
      );
 }
