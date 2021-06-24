50099b942d7de9a93a5ff1d1e6d5ea81f43be200
Ivan Chaus <ivan13oct@gmail.com>
2021-06-24 17:26:14 +0300

    #26 post details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,22 @@
+import { div } from "prelude-ls";
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch'
 
 const BlogDetails = () => {
     const { id } = useParams();
-    
+    const { data: post, isPending, error}=useFetch("http://localhost:8000/blogs/"+id); 
+
     return ( 
         <div className="blog-details">
-            <h2> Blog details - {id} </h2>
+            { isPending && <div>Loading...</div>}
+            { error && <div> Error: {error} </div>}
+            { post &&(
+                <article>
+                    <h2> Post id - {post.id} </h2>
+                    <h3>Posted by: {post.author}</h3>
+                    <p> {post.body} </p>
+                </article>)
+            }
         </div>
      );
 }
