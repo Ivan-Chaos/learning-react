42072400bb42b1f99e9a9fa7038d457912621300
Ivan Chaus <ivan13oct@gmail.com>
2021-06-24 21:29:14 +0300

    #31 delete requests

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,19 @@
-import { div } from "prelude-ls";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from './useFetch'
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: post, isPending, error}=useFetch("http://localhost:8000/blogs/"+id); 
+    const history = useHistory();
+
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/'+post.id, {
+            method: 'DELETE'
+        })
+        .then(() => {
+            history.push('/');
+        })
+    }
 
     return ( 
         <div className="blog-details">
@@ -15,6 +24,7 @@ const BlogDetails = () => {
                     <h2> Post id - {post.id} </h2>
                     <h3>Posted by: {post.author}</h3>
                     <p> {post.body} </p>
+                    <button onClick={handleDelete}>Delete!</button>
                 </article>)
             }
         </div>
