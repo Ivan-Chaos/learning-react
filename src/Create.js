593e40af17e15caa99a3296cdb9ec5cc1a4c317c
Ivan Chaus <ivan13oct@gmail.com>
2021-06-24 21:19:31 +0300

    #30 useHistory hook

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,41 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle]=useState('');
     const [body, setBody]=useState('');
     const [author, setAuthor]=useState('gayboy');
+
+    const [isPending, setIsPending]= useState(false);
+    
+    const history = useHistory();
+
+
+    const handleSumbit = (event) =>{
+        event.preventDefault();
+        const post ={title, body, author};
+        
+        //post request 
+
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify(post)
+        }).then(() =>{
+            console.log("new post uploaded");
+            setIsPending(false);
+            //history.go(-1);
+            history.push('/');
+        })
+
+        
+    }
     
     return ( 
         <div className="create">
             <h2>Add a new baguette</h2>
-            <form>
+            <form onSubmit = {handleSumbit} >
                 <label>Post title</label>
                 <input 
                     type="text"
@@ -32,7 +59,8 @@ const Create = () => {
                     <option value="gayboy">Gayboy</option>
                     <option value="straightboy">Straightboy</option>
                 </select>
-                <button type="submit">Post!</button>
+                {!isPending && <button type="submit">Post!</button>}
+                { isPending && <button disabled type="submit">Posting...</button>}
             
             </form>
         </div>
